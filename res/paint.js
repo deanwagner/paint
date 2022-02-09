6aca2ca1d5c2d14ab23ffde43d425f38d1402b8f
Dean Wagner <me@deanwagner.net>
2022-02-08 16:19:00 -0800

    added LocalStorage support for saving image

--- res/paint.js
+++ res/paint.js
@@ -25,11 +25,29 @@ class Paint {
         this.canvasSlider = document.getElementById('canvas-size');
         this.range        = document.getElementById('range');
 
+        // LocalStorage
+        this.storage = window.localStorage;
+
+        // Check for Stored Image Size
+        if (this.storage.hasOwnProperty('size')) {
+            this.pixels = parseInt(this.storage.getItem('size'));
+        }
+
         // Set Defaults
         this.setDefaults();
 
         // Build Grid
         this.buildGrid();
+
+        // Check for Existing Image
+        if (this.storage.hasOwnProperty('matrix')) {
+            // Build Image from Stored Matrix
+            const matrix = JSON.parse(this.storage.getItem('matrix'));
+            const divs = this.canvas.getElementsByTagName('div');
+            for (let i = 0; i < divs.length; i++) {
+                divs[i].style.backgroundColor = matrix[i];
+            }
+        }
 
         // Mouse State Tracker
         document.body.addEventListener('mousedown', () => {
@@ -78,13 +96,57 @@ class Paint {
             document.documentElement.style.setProperty('--grid-size', size);
         });
 
-        // Clear Image
+        // Clear Canvas
         document.getElementById('clear').addEventListener('click', (e) => {
             e.preventDefault();
             const divs = this.canvas.getElementsByTagName('div');
             for (let i = 0; i < divs.length; i++) {
                 divs[i].style.backgroundColor = null;
             }
+        });
+
+        // Modal Close Button <X>
+        const close = document.querySelectorAll('.close_modal');
+        for (let i = 0; i < close.length; i++) {
+            close[i].addEventListener('click', (e) => {
+                e.preventDefault();
+                this.closeModal(e.currentTarget.dataset.id);
+            });
+        }
+
+        // <Save> Button
+        document.getElementById('button_save').addEventListener('click', (e) => {
+            e.preventDefault();
+
+            // Check for Existing Image
+            if (!this.storage.hasOwnProperty('size') || !this.storage.hasOwnProperty('matrix')) {
+                // Show Modal for First Save
+                this.openModal('save_modal');
+            }
+
+            // Build Matrix
+            const matrix = [];
+            const divs = this.canvas.getElementsByTagName('div');
+            for (let i = 0; i < divs.length; i++) {
+                matrix[i] = (divs[i].style.backgroundColor) ? divs[i].style.backgroundColor : null;
+            }
+
+            // Store Image
+            this.storage.setItem('matrix', JSON.stringify(matrix));
+            this.storage.setItem('size', this.canvasSlider.value);
+            console.log('Image Saved');
+        });
+
+        // Save <OK> Button
+        document.getElementById('save_confirm').addEventListener('click', (e) => {
+            e.preventDefault();
+            this.closeModal('save_modal');
+        });
+
+        // <Export> Button
+        document.getElementById('button_export').addEventListener('click', (e) => {
+            e.preventDefault();
+            this.openModal('export_modal');
         });
     }
 
@@ -145,7 +207,17 @@ class Paint {
         const r = Math.floor(Math.random() * 256);
         const g = Math.floor(Math.random() * 256);
         const b = Math.floor(Math.random() * 256);
-        return `rgb(${r}, ${g}, ${b})`;
+        return '#' + this.byteToHex(r) + this.byteToHex(g) + this.byteToHex(b);
+    }
+
+    /**
+     * Convert RGB byte to Hex
+     * @param   {number} byte - 0-255 Int
+     * @returns {string} - Hex
+     */
+    byteToHex(byte) {
+        const hex = byte.toString(16);
+        return (hex.length === 1) ? '0' + hex : hex;
     }
 
     /**
@@ -158,8 +230,32 @@ class Paint {
         this.canvasPicker.value = '#ffffff';
 
         // Default Values
-        this.range.value = this.pixels;
+        this.canvasSlider.value = this.pixels;
         document.getElementById('paint-custom').checked = true;
         document.getElementById('grid-show').checked = true;
+    }
+
+    /**
+     * Open Modal
+     * @param {string} id - Modal ID
+     */
+    openModal(id) {
+        const mask  = document.getElementById('modal');
+        const modal = document.getElementById(id);
+        modal.style.display = 'block';
+        mask.style.display = 'flex';
+        mask.style.opacity = '1';
+    }
+
+    /**
+     * Close Modal
+     * @param {string} id - Modal ID
+     */
+    closeModal(id) {
+        const mask  = document.getElementById('modal');
+        const modal = document.getElementById(id);
+        mask.style.opacity = '0';
+        mask.style.display = 'none';
+        modal.style.display = 'none';
     }
 }
