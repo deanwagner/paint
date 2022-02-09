2eb0e902f0c2ed8e30e67cd12e9ed58ec4043f55
Dean Wagner <me@deanwagner.net>
2022-02-09 10:08:54 -0800

    missing ;

--- res/paint.js
+++ res/paint.js
@@ -273,7 +273,7 @@ class Paint {
         const r = Math.floor(Math.random() * 256);
         const g = Math.floor(Math.random() * 256);
         const b = Math.floor(Math.random() * 256);
-        return `rgb(${r}, ${g}, ${b})`
+        return `rgb(${r}, ${g}, ${b})`;
     }
 
     /* * * * * NO LONGER USED * * * * * *\
